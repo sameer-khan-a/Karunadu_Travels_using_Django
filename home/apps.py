1ccef86c4e2b08a2bbec49819ef87a68ecbc1332
Sameer Khan A <sameerkhan003a@gmail.com>
2026-07-23 15:59:26 +0530

    Implement superuser creation in HomeConfig
    Add a ready method to create a superuser if it doesn't exist.

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
 
-
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
+
+    def ready(self):
+        try:
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+
+            if not User.objects.filter(username="sameer").exists():
+                User.objects.create_superuser(
+                    username="sameer",
+                    email="sameer@gmail.com",
+                    password="your_secure_password"
+                )
+        except Exception:
+            pass
