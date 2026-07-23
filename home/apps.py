a17d06f58c717e83a0d220703b2cb7fbca33aca6
Sameer Khan A <sameerkhan003a@gmail.com>
2026-07-23 15:44:37 +0530

    Remove superuser creation logic from HomeConfig
    Removed the ready method that creates a superuser if it doesn't exist.

--- home/apps.py
+++ home/apps.py
@@ -3,19 +3,3 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
-
-    def ready(self):
-        try:
-            from django.contrib.auth import get_user_model
-
-            User = get_user_model()
-
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="sameer",
-                    email="sameer@gmail.com",
-                    password="sameerkhan@123"
-                )
-                print("✓ Admin created.")
-        except Exception:
-            pass
